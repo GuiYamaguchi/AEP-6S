e366dade9efd110ccc4d42a6d4f2efd726388d49
Gabriel Campos Feiten <gabrielcamposfeiten@gmail.com>
2026-09-03 14:43:47 -0300

    DTO adicionado + cursoId autoincremental (v0.6)

--- cursoLivre/src/main/java/com/aep/cursoLivre/controllers/CursoController.java
+++ cursoLivre/src/main/java/com/aep/cursoLivre/controllers/CursoController.java
@@ -1,5 +1,6 @@
 package com.aep.cursoLivre.controllers;
 
+import com.aep.cursoLivre.dtos.CursoDTO;
 import com.aep.cursoLivre.models.CursoModel;
 import com.aep.cursoLivre.services.CursoService;
 import lombok.RequiredArgsConstructor;
@@ -17,39 +18,42 @@ public class CursoController {
     private final CursoService cursoService;
 
     @GetMapping
-    public ResponseEntity<List<CursoModel>> listarCursos() {
-        List<CursoModel> cursos = cursoService.listarCursos();
+    public ResponseEntity<List<CursoDTO>> listarCursos() {
+        List<CursoDTO> cursos = cursoService.listarCursos()
+                .stream()
+                .map(CursoDTO::fromModel)
+                .toList();
         return ResponseEntity.ok(cursos);
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<CursoModel> buscarPorCursoId(@PathVariable Long id) {
-        CursoModel curso = cursoService.buscarPorCursoId(id);
-        return ResponseEntity.ok(curso);
+    @GetMapping("/{cursoId}")
+    public ResponseEntity<CursoDTO> buscarPorCursoId(@PathVariable Long cursoId) {
+        CursoModel curso = cursoService.buscarPorCursoId(cursoId);
+        return ResponseEntity.ok(CursoDTO.fromModel(curso));
     }
 
     @GetMapping("/mongo/{id}")
-    public ResponseEntity<CursoModel> buscarPorId(@PathVariable String id) {
+    public ResponseEntity<CursoDTO> buscarPorId(@PathVariable String id) {
         CursoModel curso = cursoService.buscarPorId(id);
-        return ResponseEntity.ok(curso);
+        return ResponseEntity.ok(CursoDTO.fromModel(curso));
     }
 
     @PostMapping
-    public ResponseEntity<CursoModel> criarCurso(@RequestBody CursoModel curso) {
-        CursoModel novoCurso = cursoService.criarCurso(curso);
+    public ResponseEntity<CursoDTO> criarCurso(@RequestBody CursoDTO cursoDTO) {
+        CursoModel novoCurso = cursoService.criarCurso(cursoDTO.toModel());
         URI location = URI.create("/api/cursos/" + novoCurso.getId());
-        return ResponseEntity.created(location).body(novoCurso);
+        return ResponseEntity.created(location).body(CursoDTO.fromModel(novoCurso));
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<CursoModel> atualizarCurso(@PathVariable Long id, @RequestBody CursoModel curso) {
-        CursoModel cursoAtualizado = cursoService.atualizarCurso(id, curso);
-        return ResponseEntity.ok(cursoAtualizado);
+    @PutMapping("/{cursoId}")
+    public ResponseEntity<CursoDTO> atualizarCurso(@PathVariable Long cursoId, @RequestBody CursoDTO cursoDTO) {
+        CursoModel cursoAtualizado = cursoService.atualizarCurso(cursoId, cursoDTO.toModel());
+        return ResponseEntity.ok(CursoDTO.fromModel(cursoAtualizado));
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletarCurso(@PathVariable String id) {
-        cursoService.deletarCurso(id);
+    @DeleteMapping("/{cursoId}")
+    public ResponseEntity<Void> deletarCurso(@PathVariable Long cursoId) {
+        cursoService.deletarCurso(cursoId);
         return ResponseEntity.noContent().build();
     }
 }
