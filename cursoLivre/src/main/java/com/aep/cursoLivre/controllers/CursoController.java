877ac5e9e0875974e13eb541f8e7d906a8f93b14
GuiYamaguchi <japajao25@gmail.com>
2026-09-04 23:53:33 -0300

    Swagger adicionado para documentação

--- cursoLivre/src/main/java/com/aep/cursoLivre/controllers/CursoController.java
+++ cursoLivre/src/main/java/com/aep/cursoLivre/controllers/CursoController.java
@@ -2,6 +2,9 @@ package com.aep.cursoLivre.controllers;
 
 import com.aep.cursoLivre.models.CursoModel;
 import com.aep.cursoLivre.services.CursoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,24 +19,44 @@ public class CursoController {
 
     private final CursoService cursoService;
 
+    @Operation(description = "Lista todos os cursos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Todos os cursos são listados"),
+            @ApiResponse(responseCode = "400", description = "Houve um erro de syntax/payload")
+    })
     @GetMapping
     public ResponseEntity<List<CursoModel>> listarCursos() {
         List<CursoModel> cursos = cursoService.listarCursos();
         return ResponseEntity.ok(cursos);
     }
 
+    @Operation(description = "Busca o curso pelo seu cursoId")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna o curso"),
+            @ApiResponse(responseCode = "400", description = "Curso não foi encontrado")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<CursoModel> buscarPorCursoId(@PathVariable Long id) {
         CursoModel curso = cursoService.buscarPorCursoId(id);
         return ResponseEntity.ok(curso);
     }
 
+    @Operation(description = "Busca o curso pelo seu id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna o curso"),
+            @ApiResponse(responseCode = "400", description = "Curso não foi encontrado")
+    })
     @GetMapping("/mongo/{id}")
     public ResponseEntity<CursoModel> buscarPorId(@PathVariable String id) {
         CursoModel curso = cursoService.buscarPorId(id);
         return ResponseEntity.ok(curso);
     }
 
+    @Operation(description = "Chamada para criar curso")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Curso criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Curso nao foi possivel ser criado")
+    })
     @PostMapping
     public ResponseEntity<CursoModel> criarCurso(@RequestBody CursoModel curso) {
         CursoModel novoCurso = cursoService.criarCurso(curso);
@@ -41,12 +64,22 @@ public class CursoController {
         return ResponseEntity.created(location).body(novoCurso);
     }
 
+    @Operation(description = "Chamada para atualizar credenciais de um curso")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Curso atualizado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Nao foi possivel atualizar o curso")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<CursoModel> atualizarCurso(@PathVariable Long id, @RequestBody CursoModel curso) {
         CursoModel cursoAtualizado = cursoService.atualizarCurso(id, curso);
         return ResponseEntity.ok(cursoAtualizado);
     }
 
+    @Operation(description = "Chamada para deletar um curso")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Curso deletado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Nao foi possivel deletar o curso")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarCurso(@PathVariable String id) {
         cursoService.deletarCurso(id);
